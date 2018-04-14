eec5181c025810b3da8a115490e5bc43ecab75b2
Alexander Knyazev <AlexandrKnyazev1395@mail.ru>
2018-04-14 21:06:28 +0300

    import changes

--- src/blocks/CardsContainer/Card/CardsContainer-Card.js
+++ src/blocks/CardsContainer/Card/CardsContainer-Card.js
@@ -1,10 +1,7 @@
 import React, { Fragment } from 'react';
 import { decl, Bem } from 'bem-react-core';
 
-import 'm:type=smallWithImg';
-import 'm:type=smallWithText';
-import 'm:type=medium';
-import 'm:type=large';
+import 'm:type=smallWithImg | m:type=smallWithText | m:type=medium | m:type=large';
 
 export default decl({
   block: 'CardsContainer',
